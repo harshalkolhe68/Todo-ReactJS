f650ca66878d82645517c3b5bb86e3cf21c21231
harshalkolhee <105422381+harshalkolhee@users.noreply.github.com>
2024-11-02 19:23:11 +0530

    Todo Mini Project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { ToggleSwitch } from './components/ToggleSwitch'
+
+const App = () => {
+  return (
+    <div>
+      <ToggleSwitch />
+    </div>
+  )
+}
+
+export default App
